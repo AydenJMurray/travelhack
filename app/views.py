ac2dc89aae2eb379fd837523b8663d766731fb09
Ayden Murray <aydenjmurray@protonmail.com>
2017-03-25 17:24:21 +0000

    Created XML request to get all ship names, created skeleton forms

--- app/views.py
+++ app/views.py
@@ -11,7 +11,7 @@ def index():
     form = NewPostForm()
     if form.validate_on_submit:
         print form
-    data = {"data": make_request()}
+    data = {"data": make_ship_request()}
     return render_template('index.html',
                             title='Home',
                             data=data)
@@ -20,30 +20,48 @@ def index():
 def home():
     return render_template('home.html')
 
-def make_request():
-    test_request = """
-        <request>
-         <auth username="hackathon" password="pr38ns48" />
-         <method action="simplesearch" sitename="cruisedemo.traveltek.net"
-            status="Live" type="cruise">
-          <searchdetail type="cruise" startdate="2017-04-01" enddate="2017-04-30"
-            adults="2" children="0" sid="30115" resultkey="default">
-          </searchdetail>
-         </method>
-        </request>
-        """
+def make_ship_request():
+
+    line17Request = """<?xml version="1.0"?>
+    <request>
+        <auth username="hackathon" password="pr38ns48" />
+        <method action="getlinecontent" lineid="17"/>
+    </request>"""
+
+    line22Request = """<?xml version="1.0"?>
+    <request>
+        <auth username="hackathon" password="pr38ns48" />
+        <method action="getlinecontent" lineid="22"/>
+    </request>"""
 
     #print test_request
     url = "http://fusionapi.traveltek.net/0.9/interface.pl"
-    r = requests.post(url, data={"xml": test_request})
+    line17 = requests.post(url, data={"xml": line17Request})
+    line22 = requests.post(url, data={"xml": line22Request})
 
     # parse
-    root = etree.fromstring(r.text)
-    data = []
+    line17Tree = etree.fromstring(line17.text)
+    line22Tree = etree.fromstring(line22.text)
+    #print line17.text
+    #print line22.text
+
+    shipList = []
     # loop and print hotel name
-    for element in root.iterfind("results/cruise"):
-        print element.get("session")
+    for element in line17Tree.iterfind("results/line/ships/ship"):
+        #print element.get("session")
         name = element.get("name")
-        price = element.get("price")
-        data.append("{0} is {1}".format(name, price))
-    return data
+        print name
+        #desc = element.get("description")
+        shipList.append(name)
+    for element in line22Tree.iterfind("results/line/ships/ship"):
+        name=element.get("name")
+        print name
+        shipList.append(name)
+
+    shipList = sorted(shipList)
+    return shipList
+
+def make_full_search():
+    searchCriteria = "<searchdetail"
+    #region = form.data 
+    if region
