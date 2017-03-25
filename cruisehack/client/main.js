25459430fc2edaf8519b407c21ac9b0b084cbcdf
Ayden Murray <aydenjmurray@protonmail.com>
2017-03-25 14:16:55 +0000

    Created server file for making request

--- cruisehack/client/main.js
+++ cruisehack/client/main.js
@@ -1,11 +1,15 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { Mongo } from 'meteor/mongo'
 
-import './main.html';
+Router.configure({
+  layoutTemplate: "layoutDefault",
+	notFoundTemplate: "notFound",
+	loadingTemplate: "loading"
+})
 
 Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+  
 });
 
 Template.hello.helpers({
@@ -15,8 +19,7 @@ Template.hello.helpers({
 });
 
 Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+  'click button'(event) {
+
   },
 });
